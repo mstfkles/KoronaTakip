ea825db32f885692488551de6e6a3750c62335a0
Mustafa KELEŞ <76627508+Afatsum32@users.noreply.github.com>
2021-06-21 17:00:21 +0300

    Covid 19 Takib

---
+++ kaynak kod/src/screen/Yasaklar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Iletisim() {
+    return (
+       
+              <View style={{ flex: 1, justifyContent: 'space-evenly', alignItems: 'flex-start' }}>        
+            <Text>{'\n'}</Text>    
+            <Text>Yeni Tedbirler</Text>
+            <Text>  1- Hafta içi her gün gece saat 21.00 ile sabah 05.00 arasında genel sokağa çıkma sınırlaması uygulanacak.</Text>
+            <Text>  2- Sokak kısıtlaması hafta sonları cuma akşamı 21.00'den pazartesi sabahı saat 05.00'e kadar kesintisiz devam edecek.</Text>
+            <Text>  3- Daha önceki genelgelerde belirtilen istisnalar hariç, 65 yaş üstü ve 20 yaş altı, toplu taşıma araçlarını kullanamayacak.   </Text>
+            <Text>  4- Kısıtlamanın olduğu cumartesi ve pazar günleri market, bakkal, manav, kasap ve kuruyemişçiler 10.00-17.00 saatleri arasında faaliyet gösterebilecek.   </Text>
+            <Text>  5- Lokanta ve restoran tarzı iş yerleri paket servis haricinde hizmet veremeyecek.   </Text>
+            <Text>  6- Kısıtlama sürecinde (65 yaş ve üzeri ile 20 yaş altında bulunanlar hariç) unlu mamul ihtiyaçlarının karşılanması ile sınırlı olmak ve araç kullanmamak şartıyla (engelli vatandaşlar hariç) yürüme mesafesindeki fırına gidilebilecek.   </Text>
+            <Text>  7- Alışveriş merkezlerine girişte HES kodu uygulamasına geçilecek   </Text>
+            <Text>  8- Umumi Hıfzıssıhha Kurulları her alışveriş merkezi ve semt pazarı için aynı anda kabul edebilecekleri müşteri sayısını tek tek belirleyecek.</Text>
+            <Text>  9- Cenaze namazları, yakınları dahil en fazla 30 kişiyle kılınacak.</Text>
+            <Text>  10- Nikah törenlerinde de 30 kişi sınırlaması geçerli olacak.</Text>
+            <Text>  11- Evlerde gün, mevlit, taziye, yılbaşı kutlaması gibi insanların bir araya geleceği etkinlikler yapılamayacak.</Text>
+            <Text>  12- Hamam, sauna, masaj salonu, yüzme havuzu, lunapark gibi yerler faaliyetlerine ara verecek.</Text>
+            <Text>  13- Ganyan, iddia ve milli piyango bayilerinde içeriye müşteri kabul etmeksizin sadece kupon yatırma işlemi yapılabilecek.</Text>
+
+        </View>
+    )}
